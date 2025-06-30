06080eb8f9037e87abe803c8728132269924947e
Anastasiya <dunas1@yandex.ru>
2025-06-30 12:52:44 +0800

    Initial commit2

---
+++ src/main/java/ru/yandex/practicum/movierate/controller/MovieController.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.movierate.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+}
---
+++ src/main/java/ru/yandex/practicum/movierate/model/MovierateApplication.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.movierate.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierateApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MovierateApplication.class, args);
+	}
+
+}
